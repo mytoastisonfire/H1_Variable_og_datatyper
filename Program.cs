2e12c28d50413495d6a4d8457d004e3a707b4d88
mytoastisonfire <matias2305@gmail.com>
2023-08-10 13:02:51 +0200

    version 1.0

--- Program.cs
+++ Program.cs
@@ -4,13 +4,151 @@
     {
         static void Main(string[] args)
         {
-            int length = 16;
-            double width = 5.1;
-            Console.WriteLine(length * width);
-            int area = width * length;
-            //hvis man prøver at gange 16 med 5.1 så giver det et kommatal hvilket ikke kan ligge i en int
-            double d = width * length;
-            //her ganger man 16 med 5.1 igen, men ligger det ind i en double i stedet hvilket godt kan lade sig gøre
+            BlueAndRedLine();
+            Blue6StarsAndRed();
+            for(int i = 0; i < 2; i++)
+            {
+                Blue5StartsAndWhite();
+                Blue6StarsAndWhite();
+                Blue5StarsAndRed();
+                Blue6StarsAndRed();
+            }
+            BlueAndWhiteLine();
+            AllWhiteLine();
+            for(int i = 0; i < 4; i++)
+            {
+                AllRedLine();
+                AllRedLine();
+                if(i < 3)
+                {
+                    AllWhiteLine();
+                    AllWhiteLine();
+                }
+            }
+        }
+        static void Blue6StarsAndRed()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write(" ");
+            for(int i = 0; i < 5; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("*   ");
+            }
+            Console.Write("* ");
+            for(int i = 0; i < 15; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Write(" ");
+            }
+            Console.Write("\r\n");
+        }
+        static void Blue6StarsAndWhite()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write(" ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("*   ");
+            }
+            Console.Write("* ");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.Write(" ");
+            }
+            Console.Write("\r\n");
+        }
+        static void Blue5StarsAndRed()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(" ");
+                }
+                if (i < 5)
+                {
+                    Console.Write("*");
+                }
+            }
+            for(int i = 0; i < 15; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Write(" ");
+            }
+            Console.Write("\r\n");
+        }
+        static void Blue5StartsAndWhite()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(" ");
+                }
+                if (i < 5)
+                {
+                    Console.Write("*");
+                }
+            }
+            for (int i = 0; i < 15; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.Write(" ");
+            }
+            Console.Write("\r\n");
+        }
+        static void BlueAndWhiteLine()
+        {
+            for (int i = 0; i < 23; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.Write(" ");
+            }
+            for (int i = 0; i < 15; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.Write(" ");
+            }
+            Console.Write("\r\n");
+        }
+        static void BlueAndRedLine()
+        {
+            for(int i = 0; i < 23; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.Write(" ");
+            }
+            for(int i = 0; i < 15; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Write(" ");
+            }
+            Console.Write("\r\n");
+        }
+        static void AllRedLine()
+        {
+            for (int i = 0; i < 38; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Write(" ");
+            }
+            Console.Write("\r\n");
+        }
+        static void AllWhiteLine()
+        {
+            for (int i = 0; i < 38; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.Write(" ");
+            }
+            Console.Write("\r\n");
         }
     }
 }
